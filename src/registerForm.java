b0516843b5b9a11b0770a67c3ab5329586bee961
justin3527 <skykings5>
2016-11-03 19:25:45 +0900

    bts program not complete

---
+++ src/registerForm.java
@@ -0,0 +1,67 @@
+import java.awt.Frame;
+import javax.swing.JRadioButton;
+import javax.swing.JTextField;
+import javax.swing.JTextArea;
+import javax.swing.JButton;
+
+public class registerForm extends Frame {
+	
+	private String title;
+	private String contents;
+	private String supporter;
+	private String filePath;
+	/**
+	 * @wbp.nonvisual location=51,59
+	 */
+	private final JRadioButton works = new JRadioButton("works");
+	/**
+	 * @wbp.nonvisual location=141,59
+	 */
+	private final JRadioButton ncs = new JRadioButton("ncs");
+	/**
+	 * @wbp.nonvisual location=31,109
+	 */
+	private final JTextArea titleIpt = new JTextArea("title");
+	/**
+	 * @wbp.nonvisual location=21,159
+	 */
+	private final JTextArea supporterIpt = new JTextArea("supporter");
+	/**
+	 * @wbp.nonvisual location=21,219
+	 */
+	private final JTextArea contentsIpt = new JTextArea("contents");
+	/**
+	 * @wbp.nonvisual location=196,369
+	 */
+	private final JButton button = new JButton("complete");
+	
+	public registerForm()
+	{
+		//title box
+		
+		//contents box
+		
+		//supporter box
+		
+		//register button
+		
+		//Environment select radio button
+		
+	}
+	
+	public void init()
+	{
+		
+	}
+	
+	public void readFile(String path)
+	{
+		
+	}
+	
+	public void writeFile(String path)
+	{
+		
+	}
+
+}
